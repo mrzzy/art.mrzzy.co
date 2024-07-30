682feff7c0dd2a9e54741f537730b9dc7ae1b968
Zhu Zhanyan <program.nom@gmail.com>
2024-07-30 23:19:37 +0800

    test: fix parsing test due to change in price

--- lib/parsing.test.ts
+++ lib/parsing.test.ts
@@ -21,7 +21,7 @@ describe("parseArt()", () => {
       height: 410,
       id: "1",
       image: "/art/1.jpg",
-      listPrice: "100",
+      listPrice: "75",
       location: "Luggage",
       madeOn: new Date("2024-07-16T00:00:00.000Z"),
       medium: "Watercolor on paper",
