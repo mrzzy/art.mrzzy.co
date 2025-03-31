f0b2ecae8433cd9ee473975bf07d39d5c8f10a18
Zhu Zhanyan <program.nom@gmail.com>
2025-03-31 13:19:26 +0800

    test: remove flake check on csv contents in tests

--- lib/parsing.test.ts
+++ lib/parsing.test.ts
@@ -6,29 +6,10 @@
 
 import { describe, expect, test } from "@jest/globals";
 import { parseArt } from "./parsing";
-import { Orientation, Status } from "./models";
 
 describe("parseArt()", () => {
   test("Parses art.csv", async () => {
     const records = await parseArt();
     expect(records.length).toBeGreaterThan(0);
-    expect(records[0]).toStrictEqual({
-      cost: "5",
-      exhibited: null,
-      featured: false,
-      status: Status.Available,
-      framingCost: null,
-      height: 410,
-      id: "1",
-      image: "/art/1.jpg",
-      listPrice: "20",
-      location: "Home",
-      madeOn: new Date("2024-07-16T00:00:00.000Z"),
-      medium: "Watercolor on paper",
-      orientation: Orientation.Vertical,
-      sale: null,
-      title: "KL Dusk",
-      width: 310,
-    });
   });
 });
