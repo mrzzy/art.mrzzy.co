4250a1dcee93e663463459d9993dd7d9f97d38b4
Zhu Zhanyan <program.nom@gmail.com>
2026-02-06 23:52:54 +0000

    test(site): add unit test to check that parseArt() can parse public/art.csv

--- lib/parsing.test.ts
+++ lib/parsing.test.ts
@@ -13,19 +13,34 @@ import os from "os";
 describe("parseArt()", () => {
   const testPath = path.join(os.tmpdir(), "test_art.csv");
 
-  test("Parses art.csv", async () => {
-    fs.writeFileSync(testPath, `ID,Image,Orientation,Height (mm),Width (mm),Made On,Title,Medium,Location,Painting Cost,Framing Cost,List Price,Featured,Status,Exhibited,Sales Channel,Sold On,Sold Price,Bought By
+  test("Parses test_art.csv", async () => {
+    fs.writeFileSync(
+      testPath,
+      `ID,Image,Orientation,Height (mm),Width (mm),Made On,Title,Medium,Location,Painting Cost,Framing Cost,List Price,Featured,Status,Exhibited,Sales Channel,Sold On,Sold Price,Bought By
 1,/art/1.jpg,Vertical,410,310,2024-07-16,KL Dusk,Watercolor on paper,Home,5,,45,FALSE,Available,,,,,
-`, { flag: "w" });
+`,
+      { flag: "w" },
+    );
     const records = await parseArt(testPath);
     expect(records.length).toBeGreaterThan(0);
     fs.unlinkSync(testPath);
   });
 
-  test("Parses art.csv", async () => {
-    fs.writeFileSync(testPath, `,Image,Orientation,Height (mm),Width (mm),Made On,Title,Medium,Location,Painting Cost,Framing Cost,List Price,Featured,Status,Exhibited,Sales Channel,Sold On,Sold Price,Bought By
+  test("Parses full art.CSV", async () => {
+    const artCSV = path.relative(process.cwd(), "public/art.csv");
+    const records = await parseArt(artCSV);
+    expect(records.length).toBeGreaterThan(0);
+  });
+
+  test("Throws missing column test_art.csv", async () => {
+    fs.writeFileSync(
+      testPath,
+      `,Image,Orientation,Height (mm),Width (mm),Made On,Title,Medium,Location,Painting Cost,Framing Cost,List Price,Featured,Status,Exhibited,Sales Channel,Sold On,Sold Price,Bought By
+    const
 1,/art/1.jpg,Vertical,410,310,2024-07-16,KL Dusk,Watercolor on paper,Home,5,,45,FALSE,Available,,,,,
-`, { flag: "w" });
+`,
+      { flag: "w" },
+    );
     expect(parseArt(testPath)).rejects.toThrow();
     fs.unlinkSync(testPath);
   });
