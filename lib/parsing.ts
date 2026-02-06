bc4a3acdd7ca78c405b4794dc390dfa1a71e5508
Zhu Zhanyan <program.nom@gmail.com>
2026-02-06 23:50:40 +0000

    fix(web): add column and column contents to parseArt()

--- lib/parsing.ts
+++ lib/parsing.ts
@@ -9,41 +9,53 @@ import { join } from "path";
 import { Art } from "./models";
 
 /** List of expected columns in art CSV */
-const ART_CSV_COLS = [
-  "ID",
-  "Image",
-  "Orientation",
-  "Height (mm)",
-  "Width (mm)",
-  "Made On",
-  "Title",
-  "Medium",
-  "Location",
-  "Painting Cost",
-  "List Price",
-  "Featured",
-  "Status",
-  "Framing Cost",
-  "Sold On",
-  "Sales Channel",
-  "Sold Price",
-  "Bought By",
-  "Exhibited"
-];
+const ART_CSV_COLS = {
+  "ID": { required: true },
+  "Image": { required: true },
+  "Orientation": { required: true },
+  "Height (mm)": { required: true },
+  "Width (mm)": { required: true },
+  "Made On": { required: true },
+  "Title": { required: true },
+  "Medium": { required: true },
+  "Location": { required: false },
+  "Painting Cost": { required: false },
+  "List Price": { required: true },
+  "Featured": { required: true },
+  "Status": { required: true },
+  "Framing Cost": { required: false },
+  "Sold On": { required: false },
+  "Sales Channel": { required: false },
+  "Sold Price": { required: false },
+  "Bought By": { required: false },
+  "Exhibited": { required: false }
+};
 
 /**
  * Read Art metadata from CSV.
  * @param [csvPath=join(process.cwd(), "public", "art.csv")] Path to the CSV to
  * read Art metadata from.
+ * @throws Error if the CSV is invalid or required fields are missing.
  * @returns Parsed art data CSV.
  */
 export async function parseArt(
   csvPath: string = join(process.cwd(), "public", "art.csv"),
 ): Promise<Art[]> {
   return parseCSV(await readFile(csvPath), { columns: true }).map((raw: any) => {
-    if(ART_CSV_COLS.filter(c => c in raw).length !== ART_CSV_COLS.length) {
+    // check all column names are present
+    const colNames = Object.keys(ART_CSV_COLS);
+    if(colNames.filter(c => c in raw).length !== colNames.length) {
       throw new Error("Invalid art CSV: Missing columns");
     }
+
+    // check required column contents are nonempty
+    for (const [colName, { required }] of Object.entries(ART_CSV_COLS)) {
+      const value = raw[colName];
+      if (required && (value == null || value === "")) {
+        throw new Error(`Invalid art CSV: Required field '${colName}' is missing or empty`);
+      }
+    }
+
     return {
       id: raw["ID"],
       image: raw["Image"],
