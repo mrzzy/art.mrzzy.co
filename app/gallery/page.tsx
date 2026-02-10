e6188bff9be7b33a0d65a904947306bea3643903
Zhu Zhanyan <program.nom@gmail.com>
2026-02-10 14:34:31 +0000

    fix: art gallery sorting causing new art listings to be missing

--- app/gallery/page.tsx
+++ app/gallery/page.tsx
@@ -10,20 +10,20 @@ import { parseArt } from "@/lib/parsing";
 import { Metadata } from "next";
 import { Suspense } from "react";
 
-
 export const metadata: Metadata = {
   ...METADATA,
   title: "zzy Art: Gallery",
-  description: "Gallery of Watercolor paintings by Singaporean Artist Zhu Zhanyan. Available for purchase.",
-}
+  description:
+    "Gallery of Watercolor paintings by Singaporean Artist Zhu Zhanyan. Available for purchase.",
+};
 
 /**
  * Renders the Gallery Page.
  */
 export default async function GalleryPage() {
-  const pieces = (await parseArt())
-    // sort art in descending order of the date it was made
-    .sort((a, b) => b.madeOn.getTime() - a.madeOn.getTime());
+  const pieces = await parseArt();
+  // sort art in descending order of the date it was made in place
+  pieces.sort((a, b) => b.madeOn.getTime() - a.madeOn.getTime());
 
   return (
     <main className="md:my-12">
